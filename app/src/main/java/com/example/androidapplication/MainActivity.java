1d38330cfa33b90862e283ff8a082ffdd022a52f
TroyReloy1 <108343871+TroyReloy1@users.noreply.github.com>
2022-06-28 20:28:43 +0300

    Login-register
    Login and register - buttons
    ...

--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -6,8 +6,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    EditText username, password, repassword;
+    Button signup, signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,18 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(new GameView(this));
+
+        username = (EditText) findViewById(R.id.username);
+        password= (EditText)  findViewById(R.id.password);
+        repassword = (EditText) findViewById(R.id.repassword);
+        signup = (Button) findViewById(R.id.btnsignup);
+        signin = (Button) findViewById(R.id.btnsignin);
+
+
+
     }
 }
+
+
+
+
