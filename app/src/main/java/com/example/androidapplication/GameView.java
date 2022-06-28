c3e76ed10e5abd5def04789ccd09b13aae4576c8
GoldFishGlitter <46140330+GoldFishGlitter@users.noreply.github.com>
2022-06-28 15:26:22 +0300

    Created a class GameView for the main screen.

---
+++ app/src/main/java/com/example/androidapplication/GameView.java
@@ -0,0 +1,35 @@
+package com.example.androidapplication;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+import androidx.annotation.NonNull;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public GameView(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(new GameView(this));
     }
 }
